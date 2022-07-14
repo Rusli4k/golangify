9660fccc9b89ba1b34d1b9517839ca666158bf59
rusli4k <rusli@i.ua>
2022-07-14 13:18:26 +0300

    first commit

---
+++ first.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Print("My weight on Mars is:")
+	fmt.Println(102 * 0.3783)
+	fmt.Println(" my age is:")
+	fmt.Print(33 * 365 / 687)
+	fmt.Println(" years on Mars")
+}
