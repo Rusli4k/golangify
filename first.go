33adf369df9122a5b745a2b2618f2186ef6a6c3e
rusli4k <rusli@i.ua>
2022-07-14 13:22:22 +0300

    new

--- first.go
+++ first.go
@@ -4,9 +4,5 @@ import "fmt"
 
 func main() {
 
-	fmt.Print("My weight on Mars is:")
-	fmt.Println(102 * 0.3783)
-	fmt.Println(" my age is:")
-	fmt.Print(33 * 365 / 687)
-	fmt.Println(" years on Mars")
+	fmt.Print("My weight on Mars is: ", 102*0.3783, " and my age is:", 33*365/687, " years on Mars")
 }
